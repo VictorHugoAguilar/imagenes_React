db5a437e650c3ef9ee4b475082f53704bf89fae4
victor hugo <victor03016@gmail.com>
2019-08-04 11:20:33 +0200

    Mostrando la validación
    Mostrando la validación

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,49 +1,43 @@
-import React, {useState } from 'react';
+import React, { useState } from "react";
+import Error from "./Error";
 
-
-function Buscador(){
-
-    const [termino, setTermino]=useState('');
-    const [error, setError] =useState(false);
+function Buscador() {
+    const [termino, setTermino] = useState("");
+    const [error, setError] = useState(false);
 
     const buscarImagen = e => {
         e.preventDefault();
         // Validar datos
-        if(termino === ''){
+        if (termino === "") {
             setError(true);
             return;
         }
         // Enviar el termino hacia el componente principal
         setError(false);
-        
-    }
+    };
 
     return (
-        <form
-            onSubmit={buscarImagen}
-        >
+        <form onSubmit={buscarImagen}>
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Buscar una imagen, ejemplo: Futbol, Café"
-                        onChange={(e)=> setTermino(e.target.value)}
+                        onChange={e => setTermino(e.target.value)}
                     />
-
                 </div>
                 <div className="form-group col-md-4">
                     <input
-                        type="button"
+                        type="submit"
                         className="btn btn-lg btn-danger btn-block"
                         value="Buscar"
                     />
-                                    </div>
+                </div>
             </div>
+            {error ? <Error mensaje="Agregar un término de búsqueda" /> : null}
         </form>
-
     );
-
 }
 
 export default Buscador;
